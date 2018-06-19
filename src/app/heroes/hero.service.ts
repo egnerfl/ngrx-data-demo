6c3ecf85b5fce3f338ca08eae9301f2ca99dcea5
Florian Egner <f.egner@sap.com>
2018-06-19 17:43:01 +0200

    init

---
+++ src/app/heroes/hero.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { EntityServiceBase, EntityServiceFactory } from 'ngrx-data';
+import { Hero } from '../core';
+
+@Injectable()
+export class HeroService extends EntityServiceBase<Hero> {
+  constructor(entityServiceFactory: EntityServiceFactory) {
+    super('Hero', entityServiceFactory);
+  }
+}
---
+++ src/app/store/entity-store.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { EntityMetadataMap, NgrxDataModule } from 'ngrx-data';
+
+export const entityMetadata: EntityMetadataMap = {
+  Hero: {},
+  Villain: {}
+};
+
+// because the plural of "hero" is not "heros"
+export const pluralNames = { Hero: 'Heroes' };
+
+@NgModule({
+  imports: [
+    NgrxDataModule.forRoot({
+      entityMetadata: entityMetadata,
+      pluralNames: pluralNames
+    })
+  ]
+})
+export class EntityStoreModule {}
